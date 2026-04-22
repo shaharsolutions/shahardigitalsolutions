f75a57888d77f3c402e2bc38ed6eab05dd7a91d3
shaharsolutions <shaharsolutions@gmail.com>
2026-04-22 21:43:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const formMessage = document.querySelector('#formMessage');
 
     // IMPORTANT: Replace this URL with your deployed Google Apps Script Web App URL
-    const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbz3FT0as2iWi99wa531KlNu4-j5q3EV4ufJ_lPri5SjixpE8VW49JEdQkeX_P-WYS2U/exec'; 
+    const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzGDxzMDLj6iT1fDPsZcg9wKIQRFpqhiF9IAlrv4agc40q1CmxYhxOjkm1QFvkF6L0/exec'; 
 
     form.addEventListener('submit', e => {
         e.preventDefault();
